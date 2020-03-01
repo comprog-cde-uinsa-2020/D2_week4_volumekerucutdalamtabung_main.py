478183688852b7fd0fe116dacfbff605db1d5efa
Kelompok 2 Prokom D <60640030+kelompok2prokomd@users.noreply.github.com>
2020-03-01 19:59:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-import function
+import Identitas
+
 if __name__ == "__main__":
 
-    r = int(input("Masukkan jari-jari kerucut: "))
-    k = int(input("Masukkan tinggi kerucut: "))
+   listNama = ["Aizza Ilmia", "Chanda F", "Siti Badiah", "Dhewi Z", "Indah Dwi"]
+   listkelas = [ 4, 4, 4, 4, 4 ]
 
-    print ("test calling function{}". format(function.formula().add(r, k)))
+   listIdentitas = []
 
-    r = int(input("Masukkan jari-jari tabung dan kerucut: "))
-    t = int(input("Masukkan tinggi tabung: "))
+   for i in range(len(listkelas)):
+       listIdentitas.append(Identitas.Identitas(listNama[i], listkelas[i]))
 
-    print ("test calling function{}". format(function.formula().add(r, t)))
+   print("Show all {}".format(listIdentitas))
+
+   for j in range(len(listIdentitas)):
+       print(" nama: {} ".format(listIdentitas[j].show_nama()))
+       print(" kelas: {} ".format(listIdentitas[j].show_kelas()))
+
+
+
+print("Menghitung Volume Kerucut dalam Tabung")
+r = int(input("Masukkan jari-jari kerucut: "))
+k = int(input("Masukkan tinggi kerucut: "))
+
+print ("volume kerucut = {}". format(Identitas.kerucut().add(r, k)))
+
+r = int(input("Masukkan jari-jari tabung: "))
+t = int(input("Masukkan tinggi tabung: "))
+
+print ("volume tabung = {}". format(Identitas.tabung().add(r, t)))
     
